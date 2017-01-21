e581b42a728aa358b7f908111dcf9aeec35ba9ff
theTardred <holeyeahus@gmail.com>
2017-01-22 01:12:35 +0300

    added app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+})
+
+app.get('/home', function (req, res) {
+  res.send('Hello Home!');
+})
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080');
+})
